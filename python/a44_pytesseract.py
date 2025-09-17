b1b54eb9570b739512f45e031adf67990bbd0f50
freshmea <freshmea@gmail.com>
2025-09-17 16:28:14 +0900

    tesseract

--- python/a44_pytesseract.py
+++ python/a44_pytesseract.py
@@ -1,11 +1,6 @@
-
+# pip install pytesseract
 import cv2
 import pytesseract
-from PIL import Image
-
-# If you don't have tesseract executable in your PATH, include the following:
-# pytesseract.pytesseract.tesseract_cmd = r'<full_path_to_your_tesseract_executable>'
-# Example tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract'
 
 # Simple image to string
 cap = cv2.VideoCapture(4)
